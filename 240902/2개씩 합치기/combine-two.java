c8517dffe7e2b4804c5f7697c52ce2d6f3ab4a59
algoalgo <62062154+algoalgo007@users.noreply.github.com>
2024-09-02 15:11:52 +0900

    Title: 2개씩 합치기, Time: 503ms, Memory: 22MB, Status: Passed - CodeTree

--- 240902/2개씩 합치기/combine-two.java
+++ 240902/2개씩 합치기/combine-two.java
@@ -2,8 +2,9 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, ans;
-    static PriorityQueue<Integer> pq = new PriorityQueue<>();
+    static int n;
+    static Long ans = 0L;
+    static PriorityQueue<Long> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -13,13 +14,13 @@ public class Main {
 
         st = new StringTokenizer(br.readLine(), " ");
         for(int i = 0; i < n; i++) {
-            pq.offer(Integer.parseInt(st.nextToken()));
+            pq.offer(Long.parseLong(st.nextToken()));
         }
 
         while (true) {
             if(pq.size() >= 2) {
-                int a = pq.poll();
-                int b = pq.poll();
+                Long a = pq.poll();
+                Long b = pq.poll();
                 ans += (a + b);
                 pq.offer(a + b);
             } else {
