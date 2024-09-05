691e841b447d3ab0f2120d34c0dd9b4472bd8c9e
algoalgo <62062154+algoalgo007@users.noreply.github.com>
2024-09-05 11:40:37 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240905/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -0,0 +1,40 @@
+ans = []
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
+
+def bfs(x, y, dx, dy):
+    temp = 0
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if nx < 0 or  ny < 0 or nx >= n or ny >= m:
+            return
+        temp += graph[nx][ny]
+    ans.append(temp)
+
+for i in range(n):
+    for j in range(m):
+        bfs(i, j, [0, 0, 0, 0], [0, 1, 2, 3])
+        bfs(i, j, [0, 1, 2, 3], [0, 0, 0, 0])
+        bfs(i, j, [0, 0, 1, 1], [0, 1, 0, 1])
+        bfs(i, j, [0, 1, 2, 2], [0, 0, 0, 1])
+        bfs(i, j, [0, 0, 0, 1], [0, 1, 2, 0])
+        bfs(i, j, [0, 0, 1, 2], [0, 1, 1, 1])
+        bfs(i, j, [0, 1, 1, 1], [0, 0, -1, -2])
+        bfs(i, j, [0, 0, -1, -2], [0, 1, 1, 1])
+        bfs(i, j, [0, 1, 1, 1], [0, 0, 1, 2])
+        bfs(i, j, [0, 0, 1, 2], [0, 1, 0, 0])
+        bfs(i, j, [0, 0, 0, 1], [0, 1, 2, 2])
+        bfs(i, j, [0, 1, 1, 2], [0, 0, 1, 1])
+        bfs(i, j, [0, 0, -1, -1], [0, 1, 1, 2])
+        bfs(i, j, [0, -1, -1, -2], [0, 0, 1, 1])
+        bfs(i, j, [0, 0, 1, 1], [0, 1, 1, 2])
+        bfs(i, j, [0, 1, 1, 2], [0, 0, 1, 0])
+        bfs(i, j, [0, 0, 0, 2], [0, 1, 2, 1])
+        bfs(i, j, [0, 1, 1, 2], [0, 0, -1, 0])
+        bfs(i, j, [0, 0, -1, 0], [0, 1, 1, 2])
+
+ans.sort(reverse=True)
+print(ans[0])
