6c36e65307e1887eedfe6cb6427f1a3f86f5b772
algoalgo <62062154+algoalgo007@users.noreply.github.com>
2024-09-08 18:04:54 +0900

    Title: 두 수의 합, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240908/두 수의 합/sum-of-two-num.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+ans = 0
+numbers = {}
+for i in range(len(arr)):
+    if arr[i] not in numbers:
+        numbers[arr[i]] = 1
+    else:
+        numbers[arr[i]] += 1
+
+for i in range(n):
+    if k - arr[i] in numbers:
+        ans += numbers[k-arr[i]]
+        numbers[arr[i]] -= 1
+
+print(ans)
