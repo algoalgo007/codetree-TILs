acce1dcec2fc482e97000dbc81ca92be515d2bef
algoalgo <62062154+algoalgo007@users.noreply.github.com>
2024-09-24 14:46:32 +0900

    Title: 정수 선언하고 곱 출력, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240924/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        int num1 = 26;
+        int num2 = 5;
+
+        System.out.println(num1 + " * " + num2 + " = " + num1 * num2);
+    }
+}
